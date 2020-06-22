623f8ed4544e2cf8cd54ab79577df14bae9e1669
wenfangpei <peiwenfang@huawei.com>
2020-06-22 16:31:09 +0800

    Modify reason:update camke Modify content: Submitter:p00355434 Reviewer:

--- cmake/external_libs/isl.cmake
+++ cmake/external_libs/isl.cmake
@@ -1,5 +1,3 @@
-set(isl_CXXFLAGS "-D_FORTIFY_SOURCE=2 -O2")
-set(isl_CFLAGS "-D_FORTIFY_SOURCE=2 -O2")
 mindspore_add_pkg(isl
         VER 0.22
         URL http://isl.gforge.inria.fr/isl-0.22.tar.gz
