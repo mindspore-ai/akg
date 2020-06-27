1f68aa04420ad4a09779742d71bce57a9ba16d61
wenfangpei <peiwenfang@huawei.com>
2020-06-27 22:40:30 +0800

    Modify reason:adaptation and derivative work of TVM Modify content: Submitter:p00355434 Reviewer:

--- tests/fuzz/tune/autotuning/xgb_cost_model.py
+++ tests/fuzz/tune/autotuning/xgb_cost_model.py
@@ -1,3 +1,4 @@
+# This is the Python adaptation and derivative work of TVM (https://github.com/apache/incubator-tvm/).
 # Copyright 2019 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
