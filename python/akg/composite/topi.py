ed8d34ef1a94181d053e4c882262681157b2c39a
chenlei_autodiff <chenlei154@huawei.com>
2021-08-23 09:33:20 +0800

    fix strided_slice fcompute.

--- python/akg/composite/topi.py
+++ python/akg/composite/topi.py
@@ -218,7 +218,7 @@ def StridedSlice(inputs, attrs):
         new_shape = list(in_tensor.shape)
         new_shape.pop(shrink_axis)
         if not new_shape:
-            return tvm.compute([1], in_tensor)
+            return tvm.compute([1], lambda *i: in_tensor(*i))
         in_tensor = tvm.compute(new_shape, lambda *indices: in_tensor(*get_old_indices(indices, shrink_axis)))
         begin.pop(shrink_axis)
         strides.pop(shrink_axis)
