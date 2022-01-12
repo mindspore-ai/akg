06ba077eb28077084283e783bfd4eb14da5404af
hujiahui8 <hujiahui8@huawei.com>
2022-01-12 11:01:36 +0800

    Fix the bug that m and n axes tile the tail block at the same time

--- src/pass/adjust_parallel_loop.cc
+++ src/pass/adjust_parallel_loop.cc
@@ -35,46 +35,144 @@
 
 namespace akg {
 namespace ir {
+constexpr auto REPLACE_VAR = "REPLACE_VAR";
+
+class InsertIfForParallel : public IRMutator {
+ private:
+  Stmt Mutate_(const For *op, const Stmt &s) final {
+    if (op->for_type != ForType::Parallel) {
+      first_loop_var_ = 0;
+      return IRMutator::Mutate_(op, s);
+    }
+
+    if (Equal(first_loop_var_, 0)) {
+      first_loop_var_ = op->loop_var;
+    }
+
+    if (op->body.as<Block>() == nullptr) {
+      return IRMutator::Mutate_(op, s);
+    }
+
+    auto block = op->body.as<Block>();
+    auto first = block->first;
+    auto rest = block->rest;
+
+    if (first.as<For>() == nullptr || rest.as<For>() == nullptr) {
+      return IRMutator::Mutate_(op, s);
+    }
+
+    auto first_for = first.as<For>();
+    auto rest_for = rest.as<For>();
+    if (first_for->for_type != ForType::Parallel || rest_for->for_type != ForType::Serial) {
+      return IRMutator::Mutate_(op, s);
+    }
+
+    Expr condition = Mod::make(first_loop_var_, op->extent);
+    condition = EQ::make(condition, 0);
+
+    Stmt if_stmt = IfThenElse::make(condition, rest);
+    Stmt attrs_stmt = AttrStmt::make(Expr("INFO"), REPLACE_VAR, Expr(REPLACE_VAR), if_stmt);
+    Stmt block_stmt = Block::make(first, attrs_stmt);
+    first_loop_var_ = 0;
+    return For::make(op->loop_var, op->min, op->extent, op->for_type, op->device_api, block_stmt);
+  }
+
+  Expr first_loop_var_{0};
+};
 class FuseParallelLoop : public IRMutator {
  private:
   Stmt Mutate_(const For *op, const Stmt &s) final {
     if (op->for_type == ForType::Parallel) {
-      if (has_parallel_) {
-        var_extents_.Set(op->loop_var, op->extent);
+      if (parallel_nums_ > 0) {
+        value_map_[op->loop_var.get()] = op->extent;
+        ++parallel_nums_;
         return IRMutator::Mutate(op->body);
       } else {
-        has_parallel_ = true;
-        Map<Var, Expr> new_vmap;
-        std::swap(new_vmap, var_extents_);
+        ++parallel_nums_;
         Stmt body = IRMutator::Mutate(op->body);
 
-        Map<Var, Expr> vmap;
-        Expr new_extent = 1;
-        for (auto item : var_extents_) {
-          new_extent = Mul::make(new_extent, item.second);
-          vmap.Set(item.first, Mod::make(op->loop_var, item.second));
+        if (parallel_nums_ == 1) {
+          parallel_nums_ = 0;
+          value_map_.clear();
+          return For::make(op->loop_var, op->min, op->extent, op->for_type, op->device_api, op->body);
         }
 
-        vmap.Set(op->loop_var, Div::make(op->loop_var, new_extent));
+        // Calculate extent after merged parallel loop.
+        int init_i = 0;
+        Expr div_extend = 1;
+        Expr extent_sum = 1;
+        std::unordered_map<const Variable *, Expr> vmap;
+        for (auto item : value_map_) {
+          if (init_i == 0) {
+            vmap[item.first] = Mod::make(op->loop_var, item.second);
+            div_extend = Mul::make(div_extend, item.second);
+          } else {
+            auto tmp_div = Div::make(op->loop_var, div_extend);
+            vmap[item.first] = Mod::make(tmp_div, item.second);
+          }
+          extent_sum = Mul::make(extent_sum, item.second);
+        }
+        vmap[op->loop_var.get()] = Div::make(op->loop_var, extent_sum);
 
         Stmt new_body = Substitute(body, vmap);
-        std::swap(new_vmap, var_extents_);
-        has_parallel_ = false;
-        return For::make(op->loop_var, op->min, Mul::make(new_extent, op->extent), op->for_type, op->device_api,
+        parallel_nums_ = 0;
+        value_map_.clear();
+        return For::make(op->loop_var, op->min, Mul::make(extent_sum, op->extent), op->for_type, op->device_api,
                          new_body);
       }
     }
     return IRMutator::Mutate_(op, s);
   }
 
-  Var loop_var_;
-  Map<Var, Expr> var_extents_;
-  bool has_parallel_{false};
+  Stmt Mutate_(const AttrStmt *op, const Stmt &s) final {
+    if (op->attr_key == REPLACE_VAR) {
+      if (auto if_op = op->body.as<IfThenElse>()) {
+        replace_expr_.push_back(if_op->condition);
+      }
+    }
+    return IRMutator::Mutate_(op, s);
+  }
+
+  std::unordered_map<const Variable *, Expr> value_map_;
+  int parallel_nums_{0};
+  int max_parallel_{0};
+
+ public:
+  std::vector<Expr> replace_expr_;
+};
+
+class ReplaceIfForParallel : public IRMutator {
+ public:
+  explicit ReplaceIfForParallel(std::vector<Expr> &replace_expr) : replace_expr_(replace_expr) {}
+
+ private:
+  Stmt Mutate_(const AttrStmt *op, const Stmt &s) final {
+    if (op->attr_key == REPLACE_VAR) {
+      if (auto if_op = op->body.as<IfThenElse>()) {
+        CHECK(replace_expr_.size() > 0);
+
+        Expr new_condition = replace_expr_.front();
+        Stmt stmt = IfThenElse::make(new_condition, if_op->then_case, if_op->else_case);
+        replace_expr_.erase(replace_expr_.begin());
+        return stmt;
+      }
+    }
+    return IRMutator::Mutate_(op, s);
+  }
+
+ public:
+  std::vector<Expr> replace_expr_;
 };
 
 Stmt AdjustParallelLoop(const Stmt &stmt) {
+  InsertIfForParallel insert_if;
+  Stmt s = insert_if.Mutate(stmt);
+
   FuseParallelLoop fuse_parallel;
-  return fuse_parallel.Mutate(stmt);
+  s = fuse_parallel.Mutate(s);
+
+  ReplaceIfForParallel replace_parallel(fuse_parallel.replace_expr_);
+  return replace_parallel.Mutate(s);
 }
 }  // namespace ir
 }  // namespace akg
