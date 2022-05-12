c2121a181b50441e67c85c97ff1910881b6cc595
looop5 <xuhui78@huawei.com>
2022-05-12 14:54:36 +0800

    reg_op supports multiple targets

--- python/akg/ms/ops_gpu.py
+++ python/akg/ms/ops_gpu.py
@@ -22,13 +22,13 @@ from akg.ms.utils import reg_op
 @reg_op("Squeeze", utils.CUDA)
 def squeeze(x, axis=None, target=utils.CUDA):
     """Squeeze"""
-    return array.Squeeze(x, axis, target)
+    return array.squeeze(x, axis, target)
 
 
 @reg_op("SqueezeGrad", utils.CUDA)
 def squeeze_grad(y_grad, x_shape, target=utils.CUDA):
     """SqueezeGrad"""
-    return array.SqueezeGrad(y_grad, x_shape, target)
+    return array.squeeze_grad(y_grad, x_shape, target)
 
 
 @reg_op("ReLU6", utils.CUDA)
@@ -65,3 +65,33 @@ def h_sigmoid(x, target=utils.CUDA):
 def h_sigmoid_grad(y_grad, x, target=utils.CUDA):
     """HSigmoidGrad"""
     return nn.HSigmoidGrad(y_grad, x, target)
+
+
+@reg_op("GatherNd", utils.CUDA)
+def gather_nd(data, indices, target=utils.CUDA):
+    """GatherNd"""
+    return array.gather_nd(data, indices, target)
+
+
+@reg_op("Gather", utils.CUDA)
+def gather(data, indices, axis, target=utils.CUDA):
+    """Gather"""
+    return array.gather(data, indices, axis, target)
+
+
+@reg_op("OneHot", utils.CUDA)
+def one_hot(indices, on_value, off_value, depth, axis, dtype, target=utils.CUDA):
+    """OneHot"""
+    return array.one_hot(indices, on_value, off_value, depth, axis, dtype, target)
+
+
+@reg_op("StandardNormal", utils.CUDA)
+def standard_normal(seed, shape, target=utils.CUDA):
+    """StandardNormal"""
+    return array.standard_normal(seed, shape, target)
+
+
+@reg_op("TensorScatterAdd", utils.CUDA)
+def tensor_scatter_add(data, indices, updates, target=utils.CUDA):
+    """TensorScatterAdd"""
+    return array.tensor_scatter_add(data, indices, updates, target)
