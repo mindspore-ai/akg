8cd10485f81d2207c69fdc5f2ae36d9f8767d902
huawuyi <liuxuehui4@huawei.com>
2026-01-05 10:58:21 +0800

    [AKG] Fix AKG Profiling.

--- akg-mlir/python/akg_mlir/ascend_profilier/path_manager.py
+++ akg-mlir/python/akg_mlir/ascend_profilier/path_manager.py
@@ -1,15 +1,22 @@
+"""Path manager module for handling profiling paths."""
 import os
 import re
 
+
 class PathManager:
+    """Manager for profiling-related paths."""
 
     @classmethod
     def get_cann_path(cls, profiler_path: str) -> str:
+        """Get CANN profiling path from profiler directory."""
         sub_dirs = os.listdir(os.path.realpath(profiler_path))
         sub_dirs.sort(reverse=True)
         for sub_dir in sub_dirs:
             sub_path = os.path.join(profiler_path, sub_dir)
-            if os.path.isdir(sub_path) and (re.match(r"^PROF_\d+_\d+_\d+[a-zA-Z]+", sub_dir) or re.match(r"^PROF_\d+_\d+[a-zA-Z]+", sub_dir)):
+            if os.path.isdir(sub_path) and (
+                re.match(r"^PROF_\d+_\d+_\d+_[a-zA-Z]+", sub_dir) or
+                re.match(r"^PROF_\d+_\d+_[a-zA-Z]+", sub_dir)
+            ):
                 return sub_path
         return ""
 
@@ -24,6 +31,7 @@ class PathManager:
 
     @classmethod
     def get_start_info_path(cls, cann_path: str) -> str:
+        """Get start info file path from CANN directory."""
         start_info_path = os.path.join(cann_path, "host", "start_info")
         if os.path.exists(start_info_path):
             return start_info_path
@@ -41,6 +49,7 @@ class PathManager:
 
     @classmethod
     def get_device_all_file_list_by_type(cls, profiler_path: str, summary_or_timeline: str) -> list:
+        """Get all files from device directory by type (summary or timeline)."""
         file_list = []
         _path = os.path.join(cls.get_device_path(profiler_path), summary_or_timeline)
         if not os.path.isdir(_path):
@@ -54,6 +63,7 @@ class PathManager:
 
     @classmethod
     def get_analyze_all_file(cls, profiler_path: str, analyze: str) -> list:
+        """Get all files from analyze directory."""
         file_list = []
         _path = os.path.join(profiler_path, analyze)
         if not os.path.isdir(_path):
