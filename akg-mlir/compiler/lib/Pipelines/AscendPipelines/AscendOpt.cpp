81ab3ab4002b942773d789cc0704529c4038c649
liuchao <liuchao195@huawei.com>
2026-01-13 11:08:31 +0800

    refactor affine to scf pass

--- akg-mlir/compiler/lib/Pipelines/AscendPipelines/AscendOpt.cpp
+++ akg-mlir/compiler/lib/Pipelines/AscendPipelines/AscendOpt.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright 2024-2025 Huawei Technologies Co., Ltd
+ * Copyright 2024-2026 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -113,10 +113,8 @@ void createAscendOptPipelineImpl(OpPassManager &pm, const mlir::AscendOptPipelin
     nestedFusionPM.addPass(mlir::createAffineIteratorConversionPass());
     nestedFusionPM.addPass(mlir::createBF16ToF32Pass());
     nestedFusionPM.addPass(mlir::createConvertAffineToSCFPass());
-    nestedFusionPM.addPass(mlir::createLowerAffinePass());
 
     pm.addPass(mlir::createAddOutParameterPass());
-
     pm.addPass(mlir::createLegalizeBoolPass());
     // tiling
     pm.addPass(mlir::createNPUAutoTilingPass());
