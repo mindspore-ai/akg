123c7445606d7b400d0d6d017177d349e0e6de3a
lihui <lihui488@huawei.com>
2026-01-15 17:25:51 +0800

    fix legalize bool pass
    Signed-off-by: lihui <lihui488@huawei.com>

--- akg-mlir/compiler/lib/Dialect/Affine/Transforms/LegalizeBool.cpp
+++ akg-mlir/compiler/lib/Dialect/Affine/Transforms/LegalizeBool.cpp
@@ -83,6 +83,14 @@ static Value createI8ToI1ScalarCast(Location loc, Value input, OpBuilder &builde
   return builder.create<arith::CmpFOp>(loc, arith::CmpFPredicate::ONE, inputF16, zeroF16);
 }
 
+struct LegalizeBoolIntBinaryOpMatcher {
+  static bool match(Operation *op) {
+    return isa<arith::AddIOp, arith::SubIOp, arith::MulIOp, arith::DivSIOp, arith::DivUIOp, arith::RemSIOp,
+               arith::RemUIOp, arith::MaxSIOp, arith::MaxUIOp, arith::MinSIOp, arith::MinUIOp, arith::ShLIOp,
+               arith::ShRSIOp, arith::ShRUIOp, arith::AndIOp, arith::OrIOp, arith::XOrIOp>(op);
+  }
+};
+
 static Value createI1ToI8MemRefCast(Location loc, Value input, MemRefType expectedType,
                                    OpBuilder &builder) {
   if (input.getType() == expectedType) {
@@ -102,6 +110,67 @@ static Value createI1ToI8MemRefCast(Location loc, Value input, MemRefType expect
   auto castOp = builder.create<UnrealizedConversionCastOp>(loc, TypeRange{expectedType},
                                                           ValueRange{input});
   return castOp.getResult(0);
+}
+
+static bool isI1OrI8IntType(Type type) {
+  return type.isInteger(1) || type.isInteger(8);
+}
+
+static void legalizeIntBinaryOperandsAndResult(Operation *op, OpBuilder &builder) {
+  if (op->getNumOperands() != 2 || op->getNumResults() != 1) {
+    return;
+  }
+
+  Value lhs = op->getOperand(0);
+  Value rhs = op->getOperand(1);
+  Type lhsType = lhs.getType();
+  Type rhsType = rhs.getType();
+  if (!isI1OrI8IntType(lhsType) || !isI1OrI8IntType(rhsType)) {
+    return;
+  }
+
+  Type desiredType;
+  if (lhsType == rhsType) {
+    desiredType = lhsType;
+  } else if (lhsType.isInteger(8) && rhsType.isInteger(1)) {
+    desiredType = lhsType;
+    rhs = createI1ToI8ScalarCast(op->getLoc(), rhs, builder);
+    op->setOperand(1, rhs);
+  } else if (lhsType.isInteger(1) && rhsType.isInteger(8)) {
+    desiredType = rhsType;
+    lhs = createI1ToI8ScalarCast(op->getLoc(), lhs, builder);
+    op->setOperand(0, lhs);
+  } else {
+    return;
+  }
+
+  if (op->getResult(0).getType() != desiredType) {
+    op->getResult(0).setType(desiredType);
+  }
+}
+
+static void legalizeCmpIOperands(Operation *op, OpBuilder &builder) {
+  auto cmpOp = dyn_cast<arith::CmpIOp>(op);
+  if (!cmpOp || cmpOp.getNumOperands() != 2) {
+    return;
+  }
+  Value lhs = cmpOp.getLhs();
+  Value rhs = cmpOp.getRhs();
+  Type lhsType = lhs.getType();
+  Type rhsType = rhs.getType();
+  if (lhsType == rhsType) {
+    return;
+  }
+  if (lhsType.isInteger(8) && rhsType.isInteger(1)) {
+    rhs = createI1ToI8ScalarCast(op->getLoc(), rhs, builder);
+    cmpOp->setOperand(1, rhs);
+    return;
+  }
+  if (lhsType.isInteger(1) && rhsType.isInteger(8)) {
+    lhs = createI1ToI8ScalarCast(op->getLoc(), lhs, builder);
+    cmpOp->setOperand(0, lhs);
+    return;
+  }
 }
 
 static void updateFunctionType(func::FuncOp func) {
@@ -134,84 +203,159 @@ static void updateFunctionType(func::FuncOp func) {
   }
 }
 
-static void legalizeLoadOps(func::FuncOp func) {
-  func.walk([&](memref::LoadOp loadOp) {
-    auto memrefType = dyn_cast<MemRefType>(loadOp.getMemRef().getType());
-    if (!memrefType) {
-      return;
-    }
-    Type expectedType = memrefType.getElementType();
-    if (loadOp.getType() == expectedType) {
-      return;
-    }
+static WalkResult legalizeMemrefLoadOp(memref::LoadOp loadOp) {
+  auto memrefType = dyn_cast<MemRefType>(loadOp.getMemRef().getType());
+  if (!memrefType) {
+    return WalkResult::advance();
+  }
+  Type expectedType = memrefType.getElementType();
+  if (loadOp.getType() != expectedType) {
     loadOp.getResult().setType(expectedType);
-  });
+  }
+  return WalkResult::advance();
 }
 
-static void legalizeStoreOps(func::FuncOp func) {
-  func.walk([&](memref::StoreOp storeOp) {
-    auto memrefType = dyn_cast<MemRefType>(storeOp.getMemRef().getType());
-    if (!memrefType) {
-      return;
-    }
-    Type expectedType = memrefType.getElementType();
-    Value value = storeOp.getValueToStore();
-    if (value.getType() == expectedType) {
-      return;
-    }
-    if (value.getType().isInteger(1) && expectedType.isInteger(8)) {
-      OpBuilder builder(storeOp);
-      builder.setInsertionPoint(storeOp);
-      Value casted = createI1ToI8ScalarCast(storeOp.getLoc(), value, builder);
-      storeOp->setOperand(0, casted);
-    }
-  });
+static WalkResult legalizeMemrefStoreOp(memref::StoreOp storeOp) {
+  auto memrefType = dyn_cast<MemRefType>(storeOp.getMemRef().getType());
+  if (!memrefType) {
+    return WalkResult::advance();
+  }
+  Type expectedType = memrefType.getElementType();
+  Value value = storeOp.getValueToStore();
+  if (value.getType() == expectedType) {
+    return WalkResult::advance();
+  }
+  if (value.getType().isInteger(1) && expectedType.isInteger(8)) {
+    OpBuilder builder(storeOp);
+    builder.setInsertionPoint(storeOp);
+    Value casted = createI1ToI8ScalarCast(storeOp.getLoc(), value, builder);
+    storeOp->setOperand(0, casted);
+  } else if (value.getType().isInteger(8) && expectedType.isInteger(1)) {
+    OpBuilder builder(storeOp);
+    builder.setInsertionPoint(storeOp);
+    Value casted = createI8ToI1ScalarCast(storeOp.getLoc(), value, builder);
+    storeOp->setOperand(0, casted);
+  }
+  return WalkResult::advance();
 }
 
-static void legalizeSelectOps(func::FuncOp func) {
-  func.walk([&](arith::SelectOp selectOp) {
-    Value cond = selectOp.getCondition();
-    if (!cond.getType().isInteger(8)) {
-      return;
-    }
+static WalkResult legalizeMemrefExpandShapeOp(memref::ExpandShapeOp expandOp) {
+  auto srcType = dyn_cast<MemRefType>(expandOp.getSrc().getType());
+  auto dstType = dyn_cast<MemRefType>(expandOp.getResult().getType());
+  if (!srcType || !dstType) {
+    return WalkResult::advance();
+  }
+  Type expectedElemType = srcType.getElementType();
+  if (dstType.getElementType() != expectedElemType) {
+    expandOp.getResult().setType(
+        MemRefType::get(dstType.getShape(), expectedElemType, dstType.getLayout(), dstType.getMemorySpace()));
+  }
+  return WalkResult::advance();
+}
+
+static WalkResult legalizeMemrefCollapseShapeOp(memref::CollapseShapeOp collapseOp) {
+  auto srcType = dyn_cast<MemRefType>(collapseOp.getSrc().getType());
+  auto dstType = dyn_cast<MemRefType>(collapseOp.getResult().getType());
+  if (!srcType || !dstType) {
+    return WalkResult::advance();
+  }
+  Type expectedElemType = srcType.getElementType();
+  if (dstType.getElementType() != expectedElemType) {
+    collapseOp.getResult().setType(
+        MemRefType::get(dstType.getShape(), expectedElemType, dstType.getLayout(), dstType.getMemorySpace()));
+  }
+  return WalkResult::advance();
+}
+
+static WalkResult legalizeArithSelectOp(arith::SelectOp selectOp) {
+  Value cond = selectOp.getCondition();
+  if (cond.getType().isInteger(8)) {
     OpBuilder builder(selectOp);
     builder.setInsertionPoint(selectOp);
     Value i1Cond = createI8ToI1ScalarCast(selectOp.getLoc(), cond, builder);
     selectOp->setOperand(0, i1Cond);
-  });
+  }
+  return WalkResult::advance();
 }
 
-static LogicalResult legalizeReturnOps(func::FuncOp func) {
-  bool ok = true;
-  func.walk([&](func::ReturnOp returnOp) {
-    if (!ok) {
-      return;
-    }
-    OpBuilder builder(returnOp);
-    builder.setInsertionPoint(returnOp);
-    for (auto [idx, operand] : llvm::enumerate(returnOp->getOpOperands())) {
-      Type expectedType = func.getFunctionType().getResult(idx);
-      Value cur = operand.get();
-      if (cur.getType() == expectedType) {
-        continue;
-      }
+static WalkResult legalizeArithIntBinaryOp(Operation *op) {
+  OpBuilder builder(op);
+  builder.setInsertionPoint(op);
+  legalizeIntBinaryOperandsAndResult(op, builder);
+  return WalkResult::advance();
+}
 
-      Value converted;
-      if (cur.getType().isInteger(1) && expectedType.isInteger(8)) {
-        converted = createI1ToI8ScalarCast(returnOp.getLoc(), cur, builder);
-      } else if (auto expectedMemref = dyn_cast<MemRefType>(expectedType)) {
-        converted = createI1ToI8MemRefCast(returnOp.getLoc(), cur, expectedMemref, builder);
-      }
+static WalkResult legalizeArithCmpIOp(arith::CmpIOp cmpOp) {
+  OpBuilder builder(cmpOp);
+  builder.setInsertionPoint(cmpOp);
+  legalizeCmpIOperands(cmpOp, builder);
+  return WalkResult::advance();
+}
 
-      if (!converted) {
-        returnOp.emitError("unsupported i1 to i8 legalization for return operand");
-        ok = false;
-        return;
-      }
-      operand.set(converted);
+static WalkResult legalizeFuncReturnOp(func::ReturnOp returnOp, func::FuncOp func) {
+  OpBuilder builder(returnOp);
+  builder.setInsertionPoint(returnOp);
+  for (auto [idx, operand] : llvm::enumerate(returnOp->getOpOperands())) {
+    Type expectedType = func.getFunctionType().getResult(idx);
+    Value cur = operand.get();
+    if (cur.getType() == expectedType) {
+      continue;
     }
+
+    Value converted;
+    if (cur.getType().isInteger(1) && expectedType.isInteger(8)) {
+      converted = createI1ToI8ScalarCast(returnOp.getLoc(), cur, builder);
+    } else if (cur.getType().isInteger(8) && expectedType.isInteger(1)) {
+      converted = createI8ToI1ScalarCast(returnOp.getLoc(), cur, builder);
+    } else if (auto expectedMemref = dyn_cast<MemRefType>(expectedType)) {
+      converted = createI1ToI8MemRefCast(returnOp.getLoc(), cur, expectedMemref, builder);
+    }
+
+    if (!converted) {
+      return returnOp.emitError("unsupported i1 to i8 legalization for return operand"), WalkResult::interrupt();
+    }
+    operand.set(converted);
+  }
+  return WalkResult::advance();
+}
+
+static LogicalResult legalizeOpsWithSingleWalk(func::FuncOp func) {
+  WalkResult result = func.walk([&](Operation *op) -> WalkResult {
+    if (auto loadOp = dyn_cast<memref::LoadOp>(op)) {
+      return legalizeMemrefLoadOp(loadOp);
+    }
+
+    if (auto storeOp = dyn_cast<memref::StoreOp>(op)) {
+      return legalizeMemrefStoreOp(storeOp);
+    }
+
+    if (auto expandOp = dyn_cast<memref::ExpandShapeOp>(op)) {
+      return legalizeMemrefExpandShapeOp(expandOp);
+    }
+
+    if (auto collapseOp = dyn_cast<memref::CollapseShapeOp>(op)) {
+      return legalizeMemrefCollapseShapeOp(collapseOp);
+    }
+
+    if (auto selectOp = dyn_cast<arith::SelectOp>(op)) {
+      return legalizeArithSelectOp(selectOp);
+    }
+
+    if (LegalizeBoolIntBinaryOpMatcher::match(op)) {
+      return legalizeArithIntBinaryOp(op);
+    }
+
+    if (auto cmpOp = dyn_cast<arith::CmpIOp>(op)) {
+      return legalizeArithCmpIOp(cmpOp);
+    }
+
+    if (auto returnOp = dyn_cast<func::ReturnOp>(op)) {
+      return legalizeFuncReturnOp(returnOp, func);
+    }
+
+    return WalkResult::advance();
   });
-  return ok ? success() : failure();
+  return result.wasInterrupted() ? failure() : success();
 }
 
 struct LegalizeBoolPass : public impl::LegalizeBoolBase<LegalizeBoolPass> {
@@ -222,10 +366,7 @@ struct LegalizeBoolPass : public impl::LegalizeBoolBase<LegalizeBoolPass> {
     if (func.empty()) {
       return;
     }
-    legalizeLoadOps(func);
-    legalizeStoreOps(func);
-    legalizeSelectOps(func);
-    if (failed(legalizeReturnOps(func))) {
+    if (failed(legalizeOpsWithSingleWalk(func))) {
       signalPassFailure();
     }
   }
